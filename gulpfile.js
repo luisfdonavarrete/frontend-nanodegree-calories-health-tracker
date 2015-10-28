7bcbb6861870c1e6e4955f119b2758223af54756
Luis Navarrete <luis.na06@gmail.com>
2015-10-27 21:35:49 -0400

    Add minification, concatanation with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,47 @@
-var gulp = require('gulp');
+var gulp = require("gulp"), 
+    sass = require("gulp-ruby-sass"),
+    notify = require("gulp-notify"),
+    bower = require("gulp-bower"),
+    concat = require("gulp-concat"),
+    uglify = require("gulp-uglify");
 
-gulp.task("scripts", function(){
-	
+var config = {
+	sassPath: "./develoment/scss",
+	bowerDir: "./bower_components",
+	dest: "./assets"
+};
+
+/* Bower task, install all the bower dependencies */
+gulp.task("bower", function() {
+	return bower()
+		.pipe(gulp.dest(config.bowerDir))
+});
+
+/* Copy the fonts file from fontawesome to the assets directory */
+gulp.task("icons", function() {
+	return gulp.src(config.bowerDir + "/font-awesome/fonts/**.*")
+		.pipe(gulp.dest(config.dest + "/fonts"));
+});
+
+/* Complie cssc to a css file */
+gulp.task('css', function() {
+	return sass(config.sassPath + '/styles.scss', {
+		style: 'compressed',
+		loadPath: [
+			config.bowerDir,
+			config.bowerDir + '/bootstrap-sass/assets/stylesheets',
+			config.bowerDir + '/font-awesome/scss',
+		]
+	})
+	.on("error", notify.onError(function (error) {
+		return "Error: " + error.message;
+	}))
+	.pipe(gulp.dest(config.dest + "/css"));
+});
+
+/* Concat, minify javascript files */
+gulp.task("scripts",function(){
+	return gulp.src([config.bowerDir + "jquery/dist/jquery.js", "./develoment/js/*"])
+		.pipe(concat("app.js"))
+		.pipe(gulp.dest(config.dest + "/js"));
 });
